0483b48f24113759673c77d9ab786ea00b530dc0
Christian014 <christian.lopes01@hotmail.com>
2024-02-02 20:20:32 -0300

    Ordenando Palavras Csharpp

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class OrdemAlfabetica{
+    static void Main(){
+        Console.WriteLine("DIGITE UMA LISTA DE PALAVRAS QUE VOU ORDERNAR POR ORDEM ALFABÉTICA");
+
+        Console.WriteLine("Quantas palabras deseja salvar?");
+        string numPalavras = Console.ReadLine();
+
+        int numPalavrasArray = Convert.ToInt32(numPalavras);
+
+
+        string[] arrayPalavras;
+        arrayPalavras = new string[numPalavrasArray];
+
+        for(int index = 0; index < numPalavrasArray; index++){
+            Console.WriteLine($"digite: {index}");
+
+            string palavra = Console.ReadLine();
+
+            arrayPalavras[index] = palavra;
+        }
+
+        string[] arrayPalavrasOrdenadas = arrayPalavras.OrderBy(p => p).ToArray();
+
+        foreach(var i in arrayPalavrasOrdenadas){
+            Console.WriteLine(i);
+        }
+    }
+}
